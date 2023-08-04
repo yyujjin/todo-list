73f276fcb713d4e7f18e1f79dd284ed9a5ebd1e0
yyujjin <141253669+yyujjin@users.noreply.github.com>
2023-08-05 00:46:08 +0900

    아무것도 입력 안했을 때 경고창 출력

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-
+// 아무 값도 입력하지 않고 (빈문자열)버튼을 클릭하면 '할일을 입력하세요'라고 경고창 출력
 
 let input = document.querySelector("input")
 console.log(input.value)
+// let value = input.value // 10 =>input.value의 데이터 타입은 무조건 문자
+// 그래서 10을 입력해도 데이터 타입을 숫자가아닌 문자로 가지고 옴.
 let button = document.querySelector("button")
+// querySelector=>이거 쓰면 제일 처음꺼 가져옴
+//그래서 css같이 id만들어서 그걸로 연결하면 됨
+let a = ''
 const todolist = []
 button.addEventListener('click', function(){
-    todolist.push(input.value)
-    console.log(todolist)
+    if (input.value == a) {
+        alert("할 일을 입력하세요")
+    }else {
+        todolist.push(input.value) 
+        console.log(todolist)
+    }
 })
 
+
+
+// let a = ''//빈문자열 () // number, string(문자타입을) 
 
